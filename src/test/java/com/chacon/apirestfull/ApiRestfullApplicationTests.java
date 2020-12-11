22556695cbab3b624b0df4bd49b240c406e5a25d
Mateus Chacon <chacon@optimerotas.com.br>
2020-12-10 21:41:04 -0300

    first commit

---
+++ src/test/java/com/chacon/apirestfull/ApiRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chacon.apirestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
